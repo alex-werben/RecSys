b4f40000de2900ec2b1926d12785fe157b5961e4
Alex_Werben <crystall.werben@gmail.com>
2024-10-19 18:38:43 +0300

    change path for test

--- tests/test_train.py
+++ tests/test_train.py
@@ -7,10 +7,12 @@ from pipelines import train
 def test_train_e2e(mock_s3_put):
     """Tests train pipeline end-to-end and checks output existence."""
     with initialize(config_path="../configs", version_base="1.1"):
-        conf = compose(config_name="train_als", return_hydra_config=True)
+        conf = compose(config_name="train_svd", return_hydra_config=True)
 
         dir_name = conf.data.output.model_path.split('/')[0]
         os.makedirs(dir_name, exist_ok=True)
+
+        conf.data.input.interactions.path.processed = "tests/dataset_example.csv"
 
         train(conf)
 
